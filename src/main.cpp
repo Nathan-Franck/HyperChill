3a690f17be4435213ab530e932b1d17f8ca88645
TacticalDan <gorksorf@gmail.com>
2020-11-18 20:07:28 -0700

    🧪 Shader binding somewhat well defined, needs more yet

--- src/main.cpp
+++ src/main.cpp
@@ -196,7 +196,7 @@ namespace ShaderBuilder {
         string base_name = typeid(member).name();
         int last_space = std::max(
             base_name.find_last_of("::") + 1,
-            base_name.find_last_of(" "));
+            base_name.find_last_of(' '));
         return base_name.substr(last_space);
     }
 
@@ -251,22 +251,47 @@ namespace ShaderBuilder {
 //            const auto location = glGetUniformLocation(program, name.data());
 //            glUniform1fv(location, 1, &uniform);
 //        }
-        #define DEFINE_BIND(unit, value_unit, gl_call, retrieval) \
+        #define DEFINE_UNIFORM_BIND(unit, value_unit, gl_call, retrieval) \
         template<int count> \
         void bind(Uniform<GLSLUniformUnit::unit, count> member, const string& name, const value_unit& uniform) { \
             const auto location = glGetUniformLocation(program, name.data()); \
             gl_call(location, count, retrieval(uniform)); \
         }
-        DEFINE_BIND(single, float, glUniform1fv, &)
-        DEFINE_BIND(vec2, vec2, glUniform2fv, value_ptr)
-        DEFINE_BIND(vec3, vec3, glUniform3fv, value_ptr)
-        DEFINE_BIND(vec4, vec4, glUniform4fv, value_ptr)
-        DEFINE_BIND(mat4, mat4, glUniformMatrix4fv, value_ptr)
+        DEFINE_UNIFORM_BIND(single, float, glUniform1fv, &)
+        DEFINE_UNIFORM_BIND(vec2, vec2, glUniform2fv, value_ptr)
+        DEFINE_UNIFORM_BIND(vec3, vec3, glUniform3fv, value_ptr)
+        DEFINE_UNIFORM_BIND(vec4, vec4, glUniform4fv, value_ptr)
+        DEFINE_UNIFORM_BIND(mat4, mat4, glUniformMatrix4fv, value_ptr)
 
-        template<GLSLUnit unit, bool instanced>
-        void bind(Attribute<unit, instanced> member, string name) {
-
+        #define DEFINE_ATTRIBUTE_BIND(unit, value_unit) \
+        template<bool instanced, int length> \
+        void bind(Attribute<GLSLUnit::unit, instanced> member, const string& name, const array<value_unit, length>& attribute_array) { \
+            const GLint location = glGetAttribLocation((GLuint)program, (GLchar*)name.data()); \
+            GLuint vertex_buffer; \
+            glGenBuffers(1, &vertex_buffer); \
+            glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer); \
+            glBufferData(GL_ARRAY_BUFFER, attribute_array.size() * sizeof(T), &attribute_array[0], GL_STATIC_DRAW); \
+            glEnableVertexAttribArray(location); \
+            glVertexAttribPointer(location, sizeof(value_unit), GL_FLOAT, GL_FALSE, \
+                sizeof(attribute_array[0]), (void*)nullptr); \
         }
+        DEFINE_ATTRIBUTE_BIND(single, float)
+        DEFINE_ATTRIBUTE_BIND(vec2, vec2)
+        DEFINE_ATTRIBUTE_BIND(vec3, vec3)
+        DEFINE_ATTRIBUTE_BIND(vec4, vec4)
+
+        template<bool instanced> 
+        void bind_hyper(Attribute<GLSLUnit::vec3, instanced> member, const string& name, const vector<vec3>& attribute_array) { 
+            const GLint location = glGetAttribLocation((GLuint)program, (GLchar*)name.data()); 
+            GLuint vertex_buffer; 
+            glGenBuffers(1, &vertex_buffer); 
+            glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer); 
+            glBufferData(GL_ARRAY_BUFFER, attribute_array.size() * sizeof(T), &attribute_array[0], GL_STATIC_DRAW); 
+            glEnableVertexAttribArray(location); 
+            glVertexAttribPointer(location, sizeof(vec3), GL_FLOAT, GL_FALSE, 
+                sizeof(attribute_array[0]), (void*)nullptr); 
+        }
+
 
         /**
          * 📝 Write out the GLSL instructions to initialize members of
@@ -289,19 +314,22 @@ namespace ShaderBuilder {
         auto shader = Shader{vert_color{}, vert_position{}, model_view_projection()};
         shader.to_vert_text(cout);
 
-        const auto make_from = make_tuple<vector<vec2>, vector<vec3>, mat4>(
-            {
-                { -0.6f, -0.4f },
-                {  0.6f, -0.4f },
-                {   0.f,  0.6f },
-            },
-            {
-                {1.f, 1.f, 0.f},
-                {0.f, 1.f, 1.f},
-                {1.f, 0.f, 1.f},
-            },
-            {}
-        );
+        shader.bind(model_view_projection{}, "model_view_projection", mat4{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 , 13, 14, 15, 16});
+        shader.bind_hyper(vert_color{}, "vert_color", { vec3{1, 2, 3} });
+
+        // const auto make_from = make_tuple<vector<vec2>, vector<vec3>, mat4>(
+        //     {
+        //         { -0.6f, -0.4f },
+        //         {  0.6f, -0.4f },
+        //         {   0.f,  0.6f },
+        //     },
+        //     {
+        //         {1.f, 1.f, 0.f},
+        //         {0.f, 1.f, 1.f},
+        //         {1.f, 0.f, 1.f},
+        //     },7
+        //     {}
+        // );
         // shader.render(lister);
     }
 
@@ -392,15 +420,41 @@ int main()
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	std::cout << "Complete!" << std::endl;
+
+    // Here's something I haven't thought about --- 
+
+    // Being creative
+    // Having original ideas.
+
+    // There's something for you.
+
+    // The thing is creative people have energy
+    // Because they aren't focusing their energy trying to achieve someone else's arbitrary goals
+
+    // There's something there. It's not as harsh as is worded, but it's there.
+
+    // I'm a little out-of-sorts to be sure. But mostly, I just need to let my ego receed at work, let others take on responsibility, let it go for now
+    // Reduce my control over the situation, realize that there are others out there that are willing to pick up the slack
+    // Trust others to make things right.
+
+    // So this is ultimately a cooperative endeavor. A global-cooperative endeavor. We all want to succeed and will try lots and lots of
+    // Things to be able to do it. We need to facilitate the process of other people finding what they're good at, while finding what we're
+    // Good at.
+
+    // It means having a certain amount of self-confidance, but also a lot of patience and awareness of others around you.
+    // Despite what you're taught in north-american culture, ultimately, nothing can be done on your own.
+    // You are standing on the shoulders of giants, and also the sea of many.
+    // Sometimes the many is better than the giants, other times the giants are better than the many, but all lift each other up and make
+    // a greater world together. We need everyone.
+
+    // Somehow we need to improve technology enough to realize that ideal, maybe AI can help us create really nice organic, super-colonies
+    // of like-minded people. Youtube seems to be doing wonders for curating and collecting communities of people together.
+    // People are starting to low-key worship the algorithm.
+
+    // I'm not sure if I should be working on c++, it's archeic technology, we've got such better coding languages out there for high-level concepts
+    // But it is really neat for low-level data-compact performant algorithms... It's hard to know what's best. Will AI just replace all this low-level
+    // Stuff in the end?
+
+    // The job of a really good programmer is to curate and utilize pre-exising algorithms to get the job done as smoothly as possible, sounds
+    // Like something an AI could have a hand at accomplishing.
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started:
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
